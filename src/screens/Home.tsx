496950478742da6bccee5d9ea4d84f9a00308990
Gudster <gustav.fristedt@hovding.com>
2023-10-18 16:19:15 +0200

    Moved some notifee-stuff into own component
    also added pop up for notifee (importance) and pushnotificationIOS

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,100 +1,22 @@
-import * as React from 'react';
-import {Button, Text, View, PermissionsAndroid} from 'react-native';
-import PushNotification from 'react-native-push-notification';
+import React from 'react';
+import { Button, Text, View, PermissionsAndroid } from 'react-native';
 import styles from '../styles/style.js';
+import PushNotificationButton from '../components/PushNotificationButton';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-function HomeScreen({navigation}) {
-  PushNotification.createChannel(
-    {
-      channelId: '11',
-      channelName: 'Channel Name',
-      channelDescription: 'A channel for diabetes notifications',
-    },
-    created => console.log(`Channel'${created}'created`),
-  );
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+  Notifee: undefined;
+};
 
-  const requestNotificationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
-        {
-          title: 'notification Permission',
-          message: 'wants permission to send notification',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can send notifications');
-      } else {
-        console.log('Notification permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
-  const sendNotification = () => {
-    requestNotificationPermission();
-    PushNotification.createChannel;
-    PushNotification.localNotification({
-      channelId: '11',
-      title: 'dia beetus',
-      message: 'High bloodsugar',
-    });
-  };
-  // Must be outside of any component LifeCycle (such as `componentDidMount`).
-  PushNotification.configure({
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function (token) {
-      console.log('TOKEN:', token);
-    },
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
 
-    // (required) Called when a remote is received or opened, or local notification is opened
-    onNotification: function (notification) {
-      console.log('NOTIFICATION:', notification);
-
-      // process the notification
-
-      // (required) Called when a remote is received or opened, or local notification is opened
-      notification.finish(PushNotificationIOS.FetchResult.NoData);
-    },
-
-    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-    onAction: function (notification) {
-      console.log('ACTION:', notification.action);
-      console.log('NOTIFICATION:', notification);
-
-      // process the action
-    },
-
-    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-    onRegistrationError: function (err) {
-      console.error(err.message, err);
-    },
-
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-      alert: true,
-      badge: true,
-      sound: true,
-    },
-
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
-
-    /**
-     * (optional) default: true
-     * - Specified if permissions (ios) and token (android and ios) will requested or not,
-     * - if not, you must call PushNotificationsHandler.requestPermissions() later
-     * - if you are not using remote notification or do not have Firebase installed, use this:
-     *     requestPermissions: Platform.OS === 'ios'
-     */
-    requestPermissions: Platform.OS === 'ios',
-  });
-
+function HomeScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to EDU-GOTCHI</Text>
@@ -104,8 +26,8 @@ function HomeScreen({navigation}) {
           navigation.navigate('Details');
         }}
       />
-      <Button title="send girls notification" onPress={sendNotification} />
       <Button title="notifee" onPress={() => navigation.navigate('Notifee')} />
+      <PushNotificationButton />
     </View>
   );
 }
