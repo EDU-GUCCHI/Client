e415ef2c2240b5754b318e75c8f05b637fe6c1b1
Danny Gazic <dannygazic@gmail.com>
2023-10-20 10:31:35 +0200

    added enumAttributes to person

--- src/utilities/data/diagucchi.tsx
+++ src/utilities/data/diagucchi.tsx
@@ -5,14 +5,40 @@ class diagucchi
     //Person attributes
     private name: string;
     private bloodValue: number;
-    private trait: string;
-    private eatHabbit = EatingHabit.CONSISTENT;
+    private insulinPump: boolean;
+    private lchf: boolean;
     
-    public constructor(name: string, bloodValue: number, trait: string)
+    //Enum-attributes
+    private eatHabbit: EnumAttribute;
+    private exercise: EnumAttribute;
+    private weight: EnumAttribute;
+    private alcoholHabbit: EnumAttribute;
+    private smokeHabbit: EnumAttribute;
+    private illnesses: EnumAttribute[];
+
+    public constructor(
+        name: string, 
+        bloodValue: number, 
+        insulinPump: boolean,
+        lchf: boolean,
+        illnesses: EnumAttribute[], 
+        smokeHabbit: EnumAttribute, 
+        alcoholHabbit: EnumAttribute, 
+        weight: EnumAttribute, 
+        exercise: EnumAttribute, 
+        eatHabbit: EnumAttribute
+        )
     {
         this.name = name;
         this.bloodValue = bloodValue;
-        this.trait = trait;
+        this.insulinPump = insulinPump;
+        this.lchf = lchf;
+        this.illnesses = illnesses;
+        this.smokeHabbit = smokeHabbit;
+        this.alcoholHabbit = alcoholHabbit;
+        this.weight = weight;
+        this.exercise = exercise;
+        this.eatHabbit = eatHabbit;
     }
     //getters-setters
     public getName(): string
