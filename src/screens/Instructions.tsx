cbd9880570c74d90c095b5214055cea2c64ce922
oscartveit <102787591+oscartveit@users.noreply.github.com>
2023-10-24 20:22:28 +0200

    MyDay & Tutorial Screen update
    Uppdaterade MyDay och tutorial screens. Nu lite mer lika MyGotchi-screenen!

--- src/screens/Instructions.tsx
+++ src/screens/Instructions.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import styles from '../styles/style.js';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {StyleSheet} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 
 type RootStackParamList = {
-  Instructions: undefined;
-}
+  CreateGotchi: undefined;
+};
 
-type NavigationProp = StackNavigationProp<RootStackParamList, 'Instructions'>;
+type NavigationProp = StackNavigationProp<RootStackParamList, 'CreateGotchi'>;
 
 type Props = {
   navigation: NavigationProp;
-}
+};
 
-function MyDayScreen({navigation}: Props) {
+function TutorialScreen({navigation}: Props) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Instructions</Text>
+    <View style={styles.mainContainer}>
+      {/* TutorialScreen */}
+      <View style={TutorialScreenStyles.statContainerLight}>
+        <Text> Här kommer en tutorial... </Text>
+      </View>
     </View>
   );
 }
 
-export default MyDayScreen;
+export default TutorialScreen;
+
+
+
+const neonRed = '#FF2C55';
+const neonBlue = '#0AD4FA';
+const neonGreen = '#02E67E';
+const neonPink = '#FF66D9';
+const neonLight = '#E9F1F7';
+const neonDark = '#1E2532';
+
+const TutorialScreenStyles = StyleSheet.create({
+  statContainerLight: {
+    flex: 1,
+    backgroundColor: neonLight,
+    padding: 20,
+    margin: 20,
+    borderRadius: 20,
+  }
+})
--- src/screens/MyDay.tsx
+++ src/screens/MyDay.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import styles from '../styles/style.js';
 import {StackNavigationProp} from '@react-navigation/stack';
-
+import {StyleSheet} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 
 type RootStackParamList = {
-  MyDay: undefined;
+  CreateGotchi: undefined;
 };
 
-type NavigationProp = StackNavigationProp<RootStackParamList, 'MyDay'>;
+type NavigationProp = StackNavigationProp<RootStackParamList, 'CreateGotchi'>;
 
 type Props = {
   navigation: NavigationProp;
@@ -16,10 +16,71 @@ type Props = {
 
 function MyDayScreen({navigation}: Props) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>My Day</Text>
+    <View style={styles.mainContainer}>
+      {/* TutorialScreen */}
+
+      <View style={MyDayScreenStyles.statContainerLight}>
+        <Text style={MyDayScreenStyles.eventText}>Event</Text>
+        <Text style={MyDayScreenStyles.eventInformation}>13.22 - Increasingly rapid heartbeat while in the store:</Text>
+        <Text style={MyDayScreenStyles.eventSymptoms}>Symptoms: Sweating</Text>
+        <Text style={MyDayScreenStyles.eventTreatment}>Treatment: Eat a candy</Text>
+        <Text style={MyDayScreenStyles.eventCause}>Caused by: Missing a meal (lunch)</Text>
+      </View>
+
+      <View style={MyDayScreenStyles.statContainerLight}>
+        <Text style={MyDayScreenStyles.eventText}>Event</Text>
+        <Text style={MyDayScreenStyles.eventInformation}>18.43 - Low blood sugar while working out:</Text>
+        <Text style={MyDayScreenStyles.eventSymptoms}>Symptoms: Feeling dizzy</Text>
+        <Text style={MyDayScreenStyles.eventTreatment}>Treatment: Insulin shot</Text>
+        <Text style={MyDayScreenStyles.eventCause}>Caused by: Working out too hard</Text>
+      </View>
+
+      <View style={MyDayScreenStyles.statContainerLight}>
+        <Text style={MyDayScreenStyles.eventText}>Event</Text>
+        <Text style={MyDayScreenStyles.eventInformation}></Text>
+        <Text style={MyDayScreenStyles.eventSymptoms}></Text>
+        <Text style={MyDayScreenStyles.eventTreatment}></Text>
+        <Text style={MyDayScreenStyles.eventCause}></Text>
+      </View>
+
     </View>
   );
 }
 
 export default MyDayScreen;
+
+
+
+const neonRed = '#FF2C55';
+const neonBlue = '#0AD4FA';
+const neonGreen = '#02E67E';
+const neonPink = '#FF66D9';
+const neonLight = '#E9F1F7';
+const neonDark = '#1E2532';
+
+const MyDayScreenStyles = StyleSheet.create({
+  statContainerLight: {
+    flex: 1,
+    backgroundColor: neonLight,
+    padding: 20,
+    margin: 15,
+    borderRadius: 20,
+  },
+  eventText: {
+    padding: 10,
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+  eventInformation: {
+    padding: 5
+  },
+  eventSymptoms: {
+    padding: 5
+  },
+  eventTreatment: {
+    padding: 5
+  },
+  eventCause: {
+    padding: 5
+  }
+})
