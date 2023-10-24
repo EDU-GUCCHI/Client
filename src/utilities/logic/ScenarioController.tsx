4a6242d151519d64a7f8e6f114adfd3306b54714
DannysBlazingBonfire <55442971+DannysBlazingBonfire@users.noreply.github.com>
2023-10-24 12:21:32 +0200

    refactoring

--- src/utilities/logic/ScenarioController.tsx
+++ src/utilities/logic/ScenarioController.tsx
@@ -3,20 +3,6 @@ import { FormulaGenerator } from './FormulaGenerator';
 
 export class ScenarioController
 {
-
-    //private gotchi: Gotchi;
-    
-    /*
-    public constructor(gotchi: Gotchi)
-    {
-        this.gotchi = new Gotchi(); // use randomGenerator returnObject here!
-    }
-    */
-    //controlls entire flow of app logic (has access to all data modules)
-
-
-    //this section for testing code, Comment out when not testing!
-
     // has logic classes and accessed to stored data
     private formulaGenerator: FormulaGenerator;
     private storage: Storage;
