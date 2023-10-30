26ab2c0f146e4d4aa9f241c0862bed8f6756c616
Vincent <101572886+patriknotvincent@users.noreply.github.com>
2023-10-30 18:00:23 +0100

    Update, should work now

--- src/utilities/data/GotchiRandomizer.tsx
+++ src/utilities/data/GotchiRandomizer.tsx
@@ -1,73 +1,51 @@
-import {
-  Gender,
-  EatingHabit,
-  Exercise,
-  Weight,
-  AlcoholHabit,
-  SmokingHabit,
-  Illness,
-} from './EnumAttributes';
-import {Gotchi} from './Gotchi';
+import { Gender, EatingHabit, Exercise, Weight, AlcoholHabit, SmokingHabit, Illness } from './EnumAttributes';
+import { Gotchi } from './Gotchi';
 
 export function newGotchi(name: string): Gotchi {
   const random = Math.random();
 
   var gender = Gender.FEMALE;
-  var insulinPump = false;
-  var lchf = false;
-  var age = getRandomNumber(20, 80);
   var flip = coinFlip();
   if (flip === 1) {
     gender = Gender.MALE;
   }
-  flip = coinFlip();
-  if (flip === 1) {
-    insulinPump = true;
-  }
-  flip = coinFlip();
-  if (flip === 1) {
-    lchf = true;
-  }
 
-  const eatingHabits = Object.keys(EatingHabit).filter(key => isNaN(Number(key)));
-  const exerciseHabits = Object.keys(Exercise).filter(key => isNaN(Number(key)));
-  const weights = Object.keys(Weight).filter(key => isNaN(Number(key)));
-  const alcoholHabits = Object.keys(AlcoholHabit).filter(key => isNaN(Number(key)));
-  const smokingHabits = Object.keys(SmokingHabit).filter(key => isNaN(Number(key)));
-  const illnesses = Object.keys(Illness).filter(key => isNaN(Number(key)));
-
-  const chosenEatingHabit   = eatingHabits[Math.floor(random * eatingHabits.length)];
-  const chosenExerciseHabit = exerciseHabits[Math.floor(random * exerciseHabits.length)];
-  const chosenWeight        = weights[Math.floor(random * weights.length)];
-  const chosenAlcoholHabit  = alcoholHabits[Math.floor(random * alcoholHabits.length)];
-  const chosenSmokingHabit  = smokingHabits[Math.floor(random * smokingHabits.length)];
-
-  let count = Math.floor(random * illnesses.length);
-  let chosenIllnesses = [];
-  illnesses.sort(() => Math.random() - 0.5);
-  for (let i = 0; i < count; i++) {
-    chosenIllnesses.push(illnesses.pop());
+  let count = Math.floor(random * Object.keys(Illness).length);
+  let illnesses = new Set();
+  while(illnesses.size != count) {
+    illnesses.add(getRandomEnum(Illness));
   }
+  const chosenIllnesses : Illness[] = Array.from(illnesses);
+  
   return new Gotchi(
     name,
-    age,
+    getRandomNumber(20, 80),
     gender,
     8,
-    insulinPump,
-    lchf,
-    chosenEatingHabit,
-    chosenExerciseHabit,
-    chosenWeight,
-    chosenAlcoholHabit,
-    chosenSmokingHabit,
+    coinFlip() == 1,
+    coinFlip() == 1,
+    getRandomEnum(EatingHabit),
+    getRandomEnum(Exercise),
+    getRandomEnum(Weight),
+    getRandomEnum(AlcoholHabit),
+    getRandomEnum(SmokingHabit),
     chosenIllnesses,
   );
 }
 
-function coinFlip(): number {
-  return Math.round(Math.random()) + 1;
+function getRandomEnum<T>(anEnum: T): T[keyof T] {
+  const enumValues = Object.keys(anEnum)
+    .map(n => Number.parseInt(n))
+    .filter(n => !Number.isNaN(n)) as unknown as T[keyof T][];
+  const randomIndex = Math.floor(Math.random() * enumValues.length);
+  const randomEnumValue = enumValues[randomIndex];
+  return randomEnumValue;
 }
 
 function getRandomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function coinFlip(): number {
+  return Math.round(Math.random()) + 1;
 }
