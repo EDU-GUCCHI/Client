999065f943b4f7003a3b351bfc7592f080c6a272
danielols532 <danielolsson.danielolsson@gmail.com>
2023-11-02 14:01:39 +0100

    skeletor
    Just some ideas of how it could be built

--- src/utilities/data/Event.tsx
+++ src/utilities/data/Event.tsx
@@ -2,3 +2,19 @@
 
 //event type: enum (automatic event, user event)
 
+import {EventTypes} from './EventTypes';
+
+export class Event {
+  private readonly _eventType: EventTypes;
+  //What other attributes are needed? 
+  //ID? 
+  //Type of event? eg eating training etc?
+
+  public constructor(eventType: EventTypes) {
+    this._eventType = eventType;
+  }
+
+  get eventType(): EventTypes {
+    return this._eventType;
+  }
+}
--- src/utilities/logic/EventDispatcher.tsx
+++ src/utilities/logic/EventDispatcher.tsx
@@ -1,1 +1,34 @@
-// sends out Events. 
+// sends out Events.
+import {Event} from '../data/Event';
+import { Gotchi } from '../data/Gotchi';
+
+export class eventDispatcher {
+    //Should it have an instance of gotchi or should that be handled somewehere else?
+    private _gotchi : Gotchi;
+
+    public constructor(gotchi : Gotchi){
+        this._gotchi = gotchi;
+    }
+    get gotchi() : Gotchi{
+        return this._gotchi;
+    }
+
+    //Functions to create and dispatch events?
+
+    //param: information needed for event construction
+    public createEvent() {
+        //const event = new Event(...);
+    }
+
+    public dispatchEvent(event : Event) {
+        //switch case based on event type?
+        //call the appropriate event method in the cases
+    }
+
+    public eatingEvent() {
+
+    }
+    public trainingEvent() {
+
+    }
+}
