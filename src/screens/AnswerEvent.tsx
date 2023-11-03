f8246ba42eb35b282afb9b19a64bbe30998e3985
oscartveit <102787591+oscartveit@users.noreply.github.com>
2023-11-03 10:15:58 +0100

    Dropdown in AnswerEvent

--- src/screens/AnswerEvent.tsx
+++ src/screens/AnswerEvent.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import {s} from 'react-native-wind';
+import { SelectList } from 'react-native-dropdown-select-list'
 
 
 type RootStackParamList = {
@@ -14,13 +15,28 @@ type Props = {
   navigation: NavigationProp;
 };
 
+
 function AnswerEventScreen() {
-  return (
-    <View style={s`flex h-full items-center justify-center bg-coolGray-100`}>
-      <Text>AnswerEventScreen</Text>
-      <Text>Dropdowns...</Text>
+
+  const [selected, setSelected] = React.useState("");
+  const data = [
+      {key:'1', value:'Caused by', disabled:true},
+      {key:'2', value:'Träning'},
+      {key:'3', value:'Kost'},
+      {key:'4', value:'Annat :)'},
+  ]
+
+  return(
+    <View>
+      <Text style={s`text-4xl text-center m-4 mb-1 text-warmGray-600 font-semibold`}>Caused by:</Text>
+      <SelectList 
+          setSelected={(val:any) => setSelected(val)} 
+          data={data} 
+          save="value"
+      />
     </View>
   )
+    
 };
 export default AnswerEventScreen;
 
