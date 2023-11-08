2f3583675619daa91eaa1a88726819a77e5cd0d1
oscartveit <102787591+oscartveit@users.noreply.github.com>
2023-11-08 23:58:18 +0100

    Answer Event multichoicebuttons

--- src/screens/AnswerEvent.tsx
+++ src/screens/AnswerEvent.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import {s} from 'react-native-wind';
+import MultiSelectionButtons from '../components/MultiSelectionButtons';
 
 
 type RootStackParamList = {
@@ -17,11 +18,11 @@ type Props = {
 function AnswerEventScreen() {
   return (
     <View style={s`flex h-full items-center justify-center bg-coolGray-100`}>
-      <Text>AnswerEventScreen</Text>
-      <Text>Dropdowns...</Text>
+      <Text>Low bloodsugar while working out</Text>
+      <MultiSelectionButtons/>
     </View>
-  )
-};
+  );
+}
 export default AnswerEventScreen;
 
 
