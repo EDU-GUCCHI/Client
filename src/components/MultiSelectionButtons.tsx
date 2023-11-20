c6665471b16f5c961a2c596e3361c9c35ca7d5aa
Gudster <gustav.fristedt@hovding.com>
2023-11-20 08:42:57 +0100

    Update MultiSelectionButtons.tsx

--- src/components/MultiSelectionButtons.tsx
+++ src/components/MultiSelectionButtons.tsx
@@ -1,49 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
 import {s} from 'react-native-wind';
 
 const MultiSelectionButtons = () => {
-  const [selectedButtons, setSelectedButtons] = useState<string[]>([]);
-  const [buttonOptions, setButtonOptions] = useState<string[]>([]);
+  const [selectedButtons, setSelectedButtons] = useState(new Set());
+  const [submitted, setSubmitted] = useState(false);
+  const buttonOptions = [
+    'Button 1',
+    'Button 2',
+    'Button 3',
+    'Button 4',
+    'Button 5',
+    'Button 6',
+  ];
 
-  useEffect(() => {
-    // Fetch button names from the backend here and set them using setButtonOptions
-    // For example, you can use fetch or an API call to get the names.
-    const fetchedButtonNames = [
-      'Button 1',
-      'Button 2',
-      'Button 3',
-      'Button 4',
-      'Button 5',
-      'Button 6',
-    ];
-    setButtonOptions(fetchedButtonNames);
-  }, []);
-
-  const handleButtonPress = (option: string) => {
-    if (selectedButtons.includes(option)) {
-      setSelectedButtons(selectedButtons.filter(item => item !== option));
+  const handleButtonPress = option => {
+    if (submitted) return; // Disable button presses after submission
+    const newSelectedButtons = new Set(selectedButtons);
+    if (newSelectedButtons.has(option)) {
+      newSelectedButtons.delete(option);
     } else {
-      setSelectedButtons([...selectedButtons, option]);
+      newSelectedButtons.add(option);
     }
+    setSelectedButtons(newSelectedButtons);
+  };
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+
+  const getButtonStyle = option => {
+    if (submitted) {
+      if (option === 'Button 1' || option === 'Button 4') {
+        return s`bg-green-400 border-green-500 text-white`;
+      }
+      return s`bg-red-400 border-red-500 text-white`;
+    }
+    return selectedButtons.has(option)
+      ? s`bg-gray-200 border-gray-400`
+      : s`bg-warmGray-100 border-gray-300`;
   };
 
   return (
-    <View style={s`flex-row flex-wrap justify-center items-center`}>
-      {buttonOptions.map(option => (
-        <TouchableOpacity
-          key={option}
-          style={[
-            s`p-2 border border-gray-300 bg-warmGray-100 m-2`,
-            selectedButtons.includes(option) &&
-              s`bg-blue-400 border-blue-500 text-white`,
-            s`w-1/4 items-center justify-center rounded-lg`, // This will ensure at least three buttons on the same line
-          ]}
-          onPress={() => handleButtonPress(option)}>
-          <Text>{option}</Text>
-        </TouchableOpacity>
-      ))}
+    <View style={s`flex flex-col items-center`}>
+      <View style={s`flex-row flex-wrap justify-center items-center`}>
+        {buttonOptions.map(option => {
+          if (submitted && !selectedButtons.has(option)) return null; // Hide non-selected buttons after submission
+          return (
+            <TouchableOpacity
+              key={option}
+              style={[
+                s`p-2 m-2 w-1/4 items-center justify-center rounded-lg`,
+                getButtonStyle(option),
+              ]}
+              onPress={() => handleButtonPress(option)}>
+              <Text>{option}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+      <TouchableOpacity
+        style={s`bg-blue-500 p-3 rounded-lg m-2`}
+        onPress={handleSubmit}
+        disabled={submitted} // Disable submit button after submission
+      >
+        <Text style={s`text-white`}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
