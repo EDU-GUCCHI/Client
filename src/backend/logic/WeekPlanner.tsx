bb1865e7040f14a8b19e3f3a16e248e96b6f9a1f
danielols532 <danielolsson.danielolsson@gmail.com>
2023-11-22 12:35:49 +0100

    st machine

--- src/backend/logic/WeekPlanner.tsx
+++ src/backend/logic/WeekPlanner.tsx
@@ -1,14 +1,13 @@
-import { Gotchi } from "../data/gotchi/Gotchi";
-import { Storage } from "../data/Storage";
-
+import {Gotchi} from '../data/gotchi/Gotchi';
+import {Storage} from '../data/Storage';
 export class WeekPlanner {
-    private _storage: Storage;
-    private _gotchi: Gotchi;
+  private _storage: Storage;
+  private _gotchi: Gotchi;
+  private _gotchiStateMachine: GotchiStateMachine;
 
-    public constructor(storage: Storage, gotchi: Gotchi){
-        this._storage = storage;
-        this._gotchi = gotchi;
-    }
-
-
+  public constructor(storage: Storage, gotchi: Gotchi) {
+    this._storage = storage;
+    this._gotchi = gotchi;
+    this._gotchiStateMachine = new GotchiStateMachine(State.Idle);
+  }
 }
