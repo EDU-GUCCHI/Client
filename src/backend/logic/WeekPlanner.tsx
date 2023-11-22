3135266925271f8ffa6f92226ba99ac7e874d108
danielols532 <danielolsson.danielolsson@gmail.com>
2023-11-22 13:07:18 +0100

    cron schedule

--- src/backend/logic/WeekPlanner.tsx
+++ src/backend/logic/WeekPlanner.tsx
@@ -1,5 +1,7 @@
+import {Exercise} from '../data/gotchi/EnumAttributes';
 import {Gotchi} from '../data/gotchi/Gotchi';
 import {Storage} from '../data/Storage';
+import cron from 'node-cron';
 export class WeekPlanner {
   private _storage: Storage;
   private _gotchi: Gotchi;
@@ -9,5 +11,17 @@ export class WeekPlanner {
     this._storage = storage;
     this._gotchi = gotchi;
     this._gotchiStateMachine = new GotchiStateMachine(State.Idle);
+  }
+
+  //Kan testa om något sånt här fungerar, man kan schemalägga event till specifika tidpunkter
+  public scheduleEvents(): void {
+    if (this._gotchi.exercise == Exercise.ACTIVE) {
+      // Schedule exercise at 8 AM every other day, kolla node cron för syntax på schedule
+      cron.schedule('*/2 8 * * *', () => {
+        console.log('Scheduled exercise');
+        // Add event for exercise, behöver känna till eventdispatcher
+        this._gotchiStateMachine.transitionTo(State.Exercise);
+      });
+    }
   }
 }
