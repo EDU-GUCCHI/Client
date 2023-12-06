c1caae21f21fc11ee2a79ad9a6114f837f9fbd9e
Danny Gazic <dannygazic@gmail.com>
2023-12-06 12:26:27 +0100

    reformatting

--- src/backend/logic/controllers/notificationScheduler.tsx
+++ src/backend/logic/controllers/notificationScheduler.tsx
@@ -7,20 +7,23 @@ export class NotificationScheduler
     public scheduleNotifications()
     {
         // schedule notifications for a week
-        async function onCreateTriggerNotification() {
+        async function onCreateTriggerNotification() 
+        {
             const date = new Date(Date.now());
             date.setHours(11);
             date.setMinutes(10);
             // Create a time-based trigger
-            const trigger: TimestampTrigger = {
+            const trigger: TimestampTrigger = 
+            {
               type: TriggerType.TIMESTAMP,
               timestamp: date.getTime(), // fire at 11:10am (10 minutes before meeting)
             };
-            const channelId = await notifee.createChannel({
+            const channelId = await notifee.createChannel(
+              {
                 id: 'important',
                 name: 'Important Notifications',
                 importance: AndroidImportance.HIGH,
-            });
+              });
             // Create a trigger notification
             await notifee.createTriggerNotification(
               {
