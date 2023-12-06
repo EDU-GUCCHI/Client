9ffd728d77bca88aeae349806f96382c86afe700
Danny Gazic <dannygazic@gmail.com>
2023-12-06 13:18:23 +0100

    Update notificationScheduler.tsx

--- src/backend/logic/controllers/notificationScheduler.tsx
+++ src/backend/logic/controllers/notificationScheduler.tsx
@@ -39,7 +39,7 @@ export class NotificationScheduler
               trigger,
             );
           }
-          onCreateTriggerNotification();
+      onCreateTriggerNotification();
     }
     public cancelAllNotifications()
     {
