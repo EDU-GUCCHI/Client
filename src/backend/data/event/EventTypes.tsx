172865f07e271b48c2fdb29f1d996c770d1dd25b
danielols532 <danielolsson.danielolsson@gmail.com>
2023-12-08 13:00:14 +0100

    calculate form

--- src/backend/data/event/EventTypes.tsx
+++ src/backend/data/event/EventTypes.tsx
@@ -1,14 +1,15 @@
-export enum AutoType
-{
-    AUTO_EVENT, // non user interactable (eating, sleeping, working out)
-    USER_EVENT // User interactable (use insulin pump)
+export enum AutoType {
+  AUTO_EVENT, // non user interactable (eating, sleeping, working out)
+  USER_EVENT, // User interactable (use insulin pump)
 }
-export enum EventType{
-    EXERCISE,
-    FOOD_INTAKE,
-    INSULIN_INJECTION,
-    BLOOD_GLUCOSE_WARNING,
-    SLEEP,
-    NOTHING
-    //MORE?
+
+//TODO: Ge dessa värden som ska påverka blodsockerfaktorn
+export enum EventType {
+  EXERCISE,
+  FOOD_INTAKE,
+  INSULIN_INJECTION,
+  BLOOD_GLUCOSE_WARNING,
+  SLEEP,
+  NOTHING,
+  //MORE?
 }
