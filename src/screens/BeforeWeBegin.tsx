cb23faa7f74a6b3bd2a9e54997c9628854489bd4
oscartveit <102787591+oscartveit@users.noreply.github.com>
2023-12-15 14:56:36 +0100

    design

--- src/screens/BeforeWeBegin.tsx
+++ src/screens/BeforeWeBegin.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { s } from 'react-native-wind';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import ViewContainer from '../components/ViewContainer';
 import LgButton from '../components/LgButton';
 import Title from '../components/Title';
-import { View } from 'react-native';
+import BackButton from '../components/BackButton';
 
 type RootStackParamList = {
   AboutGotchi: undefined;
@@ -17,16 +17,16 @@ type Props = {
   navigation: NavigationProp;
 };
 
-// ... (previous imports and types)
-
 function BeforeWeBeginScreen({ navigation }: Props) {
   return (
-    <ViewContainer style={s`items-center justify-between h-full`}>
-      <View style={s`flex-1 justify-center`}>
+    <ViewContainer style={s`items-center justify-between h-full pt-10`}>
+      <View style={s`flex-1 justify-center p-4`}>
         <Title text="Innan vi börjar.." />
         <Text>{"Vi ber dig vänligen att ge appen tillåtelse för att skicka notifikationer. Detta för att EDU-Gotchi förlitar sig på dessa för att du som användare ska få en lärorik upplevelse"}</Text>
       </View>
-
+      <View style={s`absolute top-4 left-4 pt-12`}>
+        <BackButton />
+      </View>
       <View style={s`items-center mb-8`}>
         <LgButton
           text="Nästa"
@@ -45,6 +45,10 @@ function BeforeWeBeginScreen({ navigation }: Props) {
 }
 
 export default BeforeWeBeginScreen;
+
+
+
+
 
 
 
