5a2bafb48a60051ae854447641bbcee9dc1cbefd
Gudster <g.fristedt@gmail.com>
2023-12-21 12:46:12 +0100

    screen navigation animation flicker bug fix

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar, Platform } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {StatusBar, Platform, View} from 'react-native';
 
 import HomeScreen from './src/screens/Home';
 import CreateGotchiScreen from './src/screens/CreateGotchi';
@@ -12,7 +12,7 @@ import MyGotchiScreen from './src/screens/MyGotchi';
 import AnswerEventScreen from './src/screens/AnswerEvent';
 import AboutGotchiScreen from './src/screens/AboutGotchi';
 import BeforeWeBeginScreen from './src/screens/BeforeWeBegin';
-import { ScenarioControllerProvider } from './src/components/ScenarioControllerContext';
+import {ScenarioControllerProvider} from './src/components/ScenarioControllerContext';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,28 +20,30 @@ const App = () => {
   return (
     <ScenarioControllerProvider>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="TutorialScreen"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Tutorial" component={TutorialScreen} />
-          <Stack.Screen name="BeforeWeBegin" component={BeforeWeBeginScreen} />
-          <Stack.Screen name="CreateGotchi" component={CreateGotchiScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="MyDay" component={MyDayScreen} />
-          <Stack.Screen name="Instructions" component={InstructionsScreen} />
-          <Stack.Screen name="MyGotchi" component={MyGotchiScreen} />
-          <Stack.Screen name="AnswerEvent" component={AnswerEventScreen} />
-          <Stack.Screen name="AboutGotchi" component={AboutGotchiScreen} />
-        </Stack.Navigator>
+        <View style={{flex: 1, backgroundColor: 'white'}}>
+          <Stack.Navigator
+            initialRouteName="TutorialScreen"
+            screenOptions={{
+              headerShown: false,
+              animation: 'fade',
+            }}>
+            <Stack.Screen name="Tutorial" component={TutorialScreen} />
+            <Stack.Screen
+              name="BeforeWeBegin"
+              component={BeforeWeBeginScreen}
+            />
+            <Stack.Screen name="CreateGotchi" component={CreateGotchiScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="MyDay" component={MyDayScreen} />
+            <Stack.Screen name="Instructions" component={InstructionsScreen} />
+            <Stack.Screen name="MyGotchi" component={MyGotchiScreen} />
+            <Stack.Screen name="AnswerEvent" component={AnswerEventScreen} />
+            <Stack.Screen name="AboutGotchi" component={AboutGotchiScreen} />
+          </Stack.Navigator>
+        </View>
       </NavigationContainer>
     </ScenarioControllerProvider>
   );
 };
 
 export default App;
-
-
-
