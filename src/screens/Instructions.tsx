5448d030ee6df7a11aee02cd7290a97e9d28089c
Vincent <101572886+patriknotvincent@users.noreply.github.com>
2024-01-08 11:51:58 +0100

    Update Instructions.tsx

--- src/screens/Instructions.tsx
+++ src/screens/Instructions.tsx
@@ -27,9 +27,24 @@ function TutorialScreen({navigation}: Props) {
           Så funkar det
         </Text>
         <Text style={s`text-base text-warmGray-600`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Under loppet av en vecka inom din kurs kommer du ta hand om en Gotchi som har Typ 1 Diabetes.{"\n"} 
+        </Text>
+        <Text style={s`text-base text-warmGray-600`}>
+          Din Gotchi kommer att leva sitt egna liv men kan hamna i situationer där hen behöver din hjälp med sitt blodsocker!{"\n"} 
+        </Text>
+        <Text style={s`text-base text-warmGray-600`}>
+          När din Gotchi behöver din hjälp skickar vi ut en push-notifikation. Under "Min Dag" kan du se 
+          ett händelseflöde som visar vad din Gotchi har haft för sig.{"\n"} 
+        </Text>
+        <Text style={s`text-base text-warmGray-600`}>
+          Den senaste raden innehåller ett formulär med handlingar du kan be din Gotchi att utföra. Dessa alternativ
+          kan vara mer eller mindre relevanta beroende på situationen och kommer att återspegla hur din Gotchi mår framöver{"\n"}
+        </Text>
+        <Text style={s`text-base text-warmGray-600`}>
+          Utöver detta formulär följer ett par till som hjälper dig att studera in ditt kursmaterial.{"\n"}
+        </Text>
+        <Text style={s`text-base text-warmGray-600`}>
+          Lycka till!
         </Text>
       </View>
     </ViewContainer>
